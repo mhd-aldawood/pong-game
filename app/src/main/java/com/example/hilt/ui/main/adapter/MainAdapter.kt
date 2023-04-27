39a3ce82e3b6fe1eca676d349599f2eab3efeb62
Mohamad Aldawood <mohamad.randwork@gmail.com>
2023-04-27 17:38:13 +0400

    make recycleview use viewbinding

--- app/src/main/java/com/example/hilt/ui/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/hilt/ui/main/adapter/MainAdapter.kt
@@ -7,29 +7,23 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.hilt.R
 import com.example.hilt.data.model.User
+import com.example.hilt.databinding.ItemLayoutBinding
 import kotlinx.android.synthetic.main.item_layout.view.*
 
 class MainAdapter(
     private val users: ArrayList<User>
 ) : RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
 
-    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(user: User) {
-            itemView.textViewUserName.text = user.name
-            itemView.textViewUserEmail.text = user.email
-            Glide.with(itemView.imageViewAvatar.context)
-                .load(user.avatar)
-                .into(itemView.imageViewAvatar)
-        }
-    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        DataViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_layout, parent,
-                false
-            )
-        )
+DataViewHolder(ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        //        DataViewHolder(
+//            LayoutInflater.from(parent.context).inflate(
+//                R.layout.item_layout, parent,
+//                false
+//            )
+//        )
 
     override fun getItemCount(): Int = users.size
 
@@ -38,5 +32,14 @@ class MainAdapter(
 
     fun addData(list: List<User>) {
         users.addAll(list)
+    }
+    class DataViewHolder(itemView: ItemLayoutBinding) : RecyclerView.ViewHolder(itemView.root) {
+        fun bind(user: User) {
+            itemView.textViewUserName.text = user.name
+            itemView.textViewUserEmail.text = user.email
+            Glide.with(itemView.imageViewAvatar.context)
+                .load(user.avatar)
+                .into(itemView.imageViewAvatar)
+        }
     }
 }
